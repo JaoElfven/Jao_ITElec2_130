3748ec9566aff81279a8aee26e9067ee89ed910c
JaoElfven <princejao1993@gmail.com>
2023-02-18 17:46:52 +0800

    Initial MyApplication project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,28 +10,28 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MainActivity";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        private static final String TAG = "MainActivity";
+         @Override
+         protected void onCreate(Bundle savedInstanceState) {
+             super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
 
-        Log.v(TAG, "This is a Verbose log.");
-        Log.d(TAG, "This is a Debug log.");
-        Log.i(TAG, "This is a Info log.");
-        Log.w(TAG, "This is a Warn log.");
-        Log.e(TAG, "This is a Error log.");
+                 Log.v(TAG, "This is a Verbose log.");
+                 Log.d(TAG, "This is a Debug log.");
+                 Log.i(TAG, "This is a Info log.");
+                 Log.w(TAG, "This is a Warn log.");
+                 Log.e(TAG, "This is a Error log.");
 
-        Button button = (Button) findViewById(R.id.button);
+                 Button button = (Button) findViewById(R.id.button2);
 
-       /* button.setOnClickListener(new View.OnClickListener());{
+                /* button.setOnClickListener(new View.OnClickListener() {
 
-            @Override
-                    public  void onClick(View v){
-                Log.i(TAG, "Button Clicked");
-                Intent intent = new Intent();
-
-            }
-        }*/
-    }
+                        @Override
+                        public void onClick(View v) {
+                            Log.i(TAG, "Button Clicked");
+                            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                            startActivities(intent);
+                        }
+                 });*/
+         }
 }
